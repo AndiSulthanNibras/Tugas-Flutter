1f572a7e2f19a41beba11a688aa3271827d31eab
Andi <evoszekt2437@gmail.com>
2022-08-01 11:17:24 +0700

    Feat [SN] backbutton

--- lib/formRegistrasi.dart
+++ lib/formRegistrasi.dart
@@ -19,100 +19,103 @@ class _formRegistrasiState extends State<formRegistrasi> {
       appBar: AppBar(
         title: Text("formRegistrasi"),
       ),
-      body: Container(
-        margin: EdgeInsets.all(10),
-        child: Column(
-          children: [
-            Text(
-              "Pendaftaraan Kominfo",
-              style: const TextStyle(
-                  color: Color.fromARGB(255, 0, 0, 0),
-                  fontSize: 25,
-                  fontStyle: FontStyle.italic),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextField(
-              controller: ctrUsername,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Username",
-                hintText: "Nama kau siapa?",
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          margin: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Text(
+                "Pendaftaraan Kominfo",
+                style: const TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 25,
+                    fontStyle: FontStyle.italic),
               ),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            TextField(
-              controller: ctrPassword,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Password",
-                hintText: "Password kau apa?",
+              SizedBox(
+                height: 10,
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "Pilih jenis kelamin anda",
-              style: TextStyle(fontSize: 15),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              children: [
-                Radio(
-                  value: 1,
-                  groupValue: id,
-                  onChanged: (value) {
-                    setState(() {
-                      id = 1;
-                    });
-                  },
+              TextField(
+                controller: ctrUsername,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Username",
+                  hintText: "Nama kau siapa?",
                 ),
-                Text("Laki-Laki"),
-              ],
-            ),
-            Row(
-              children: [
-                Radio(
-                  value: 2,
-                  groupValue: id,
-                  onChanged: (value) {
-                    setState(() {
-                      id = 2;
-                    });
-                  },
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: ctrPassword,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Password",
+                  hintText: "Password kau apa?",
                 ),
-                Text("Perempuan"),
-              ],
-            ),
-            Text("phone :" + myPhone),
-            ElevatedButton(
-                onPressed: () async {
-                  final result = await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const SecondRoute()),
-                  );
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Pilih jenis kelamin anda",
+                style: TextStyle(fontSize: 15),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Radio(
+                    value: 1,
+                    groupValue: id,
+                    onChanged: (value) {
+                      setState(() {
+                        id = 1;
+                      });
+                    },
+                  ),
+                  Text("Laki-Laki"),
+                ],
+              ),
+              Row(
+                children: [
+                  Radio(
+                    value: 2,
+                    groupValue: id,
+                    onChanged: (value) {
+                      setState(() {
+                        id = 2;
+                      });
+                    },
+                  ),
+                  Text("Perempuan"),
+                ],
+              ),
+              Text("phone :" + myPhone),
+              ElevatedButton(
+                  onPressed: () async {
+                    final result = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SecondRoute()),
+                    );
 
-                  print("hasil input phone number :" + result.toString());
-                  setState(() {
-                    myPhone = result.toString();
-                  });
-                },
-                child: Text('isi nomer telepon')),
-            Center(
-              child: Container(
-                width: 200,
-                child: ElevatedButton(
-                    onPressed: () {}, child: Text("Masukan Data")),
-              ),
-            )
-          ],
+                    print("hasil input phone number :" + result.toString());
+                    setState(() {
+                      myPhone = result.toString();
+                    });
+                  },
+                  child: Text('isi nomer telepon')),
+              Center(
+                child: Container(
+                  width: 200,
+                  child: ElevatedButton(
+                      onPressed: () {}, child: Text("Masukan Data")),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
